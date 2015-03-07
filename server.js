25b04d491e5c59b75ef164553496ae6b5e7be231
D Sofer <d@sofer.com>
2015-03-07 16:29:45 +0000

    added a server message to the console

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var http = require("http");
 var handler = require("./handler");
+var port = 4000;
 var app = http.createServer(handler);
 
-app.listen(4000);
+app.listen(port);
+
+console.log('Server running on port', port);
