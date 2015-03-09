ac333e524e7576f99fa1f831bfdb0c7d5d3cbec8
gregaubs <gaj.aubert@gmail.com>
2015-03-09 15:20:19 +0000

    first push

--- handler.js
+++ handler.js
@@ -1,7 +1,42 @@
-module.exports = function handler(req, res) {
+
+module.exports = {
+
+
+	home: function handler(req, res) {
+		// console.log("Request for " + pathname + " received.");
+
+	    res.writeHead(200, {"Content-Type": "text/plain"});
+	    res.write("Welcome");
+	    res.end(" to Testing");
+
+	},
+
+	create: function handler(req, res) {
+		// console.log("Request for " + pathname + " received.");
+
+	    res.writeHead(200, {"Content-Type": "text/plain"});
+	    res.write("Welcome");
+	    res.end(" to create article");
+
+	},
+
+	update: function handler(req, res) {
+	// console.log("Request for " + pathname + " received.");
 
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.write("Welcome");
-    res.end(" to Testing");
+    res.end(" to update article");
 
-};
+	},
+
+	//createArticle
+
+	//updateArticle
+
+	//deleteArticle
+
+	//viewArticle
+
+
+}
+
--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 var http = require("http");
 var handler = require("./handler");
 var port = 4000;
-var app = http.createServer(handler);
 
-app.listen(port);
+
+//*** List of Routes and Associated Handler Functions ***//
+var routes = {}
+routes["/"] = handler.home;
+routes["/create"] = handler.create;
+routes["/update"] = handler.update;
+
+
+
+
+//*** Invokes the right handler or throws error ***//
+var router = function(req, res){
+	var url = req.url;
+	console.log("request received for ", url);
+
+
+	if (typeof routes[url] === 'function'){
+		routes[url](req, res); 
+	} else {
+		console.log('Error, route for ', url, 'does not exist');
+		res.writeHead(404, {"Content-Type": "text/plain"});
+	    res.end(" ERROR!!");
+	}
+}
+
+
+http.createServer(router).listen(port);
 
 console.log('Server running on port', port);
+
