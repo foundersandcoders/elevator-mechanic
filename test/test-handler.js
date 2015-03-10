d80e17da9ade1ff9f7eb8acc426cf50934a77753
gregaubs <gaj.aubert@gmail.com>
2015-03-10 12:18:02 +0000

    handler tests written

---
+++ test/test-handler.js
@@ -0,0 +1,89 @@
+var shot = require("shot");
+var handler = require("../handler.js");
+var assert = require("assert");
+var request;
+
+
+describe("home page response", function(){
+	it('should return 200 as the status code', function(done){
+		request = {
+			method: 'GET',
+			url: '/'
+		}
+		shot.inject(handler.home, request, function(response){
+			assert.equal(response.statusCode, 200);
+			done();
+		});
+	});
+
+	it('should return the correct string', function(done){
+		request = {
+			method: 'GET',
+			url: '/'
+		}
+
+		shot.inject(handler.home, request, function(response){
+			assert.equal(response.payload, "Welcome everybody");
+			done();
+		});
+	});
+
+});
+
+
+describe("create page response", function(){
+	it('should return 200 as the status code', function(done){
+		request = {
+			method: 'GET',
+			url: '/create'
+		}
+		shot.inject(handler.create, request, function(response){
+			assert.equal(response.statusCode, 200);
+			done();
+		});
+	});
+
+	it('should return the correct string', function(done){
+		request = {
+			method: 'GET',
+			url: '/create'
+		}
+
+		shot.inject(handler.create, request, function(response){
+			assert.equal(response.payload, "Create Article");
+			done();
+		});
+	});
+
+
+});
+
+
+
+
+describe("update page response", function(){
+	it('should return 200 as the status code', function(done){
+		request = {
+			method: 'GET',
+			url: '/update'
+		}
+		shot.inject(handler.update, request, function(response){
+			assert.equal(response.statusCode, 200);
+			done();
+		});
+	});
+
+	it('should return the correct string', function(done){
+		request = {
+			method: 'GET',
+			url: '/update'
+		}
+
+		shot.inject(handler.update, request, function(response){
+			assert.equal(response.payload, "Update Article");
+			done();
+		});
+	});
+
+
+});
