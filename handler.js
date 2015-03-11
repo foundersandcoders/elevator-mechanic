079f0dca28217ec45fa3dd86ac47a1fe3e71d7b6
Asim Javed <asim.javed@mfyp.co.uk>
2015-03-11 13:11:01 +0000

    views

--- handler.js
+++ handler.js
@@ -1,21 +1,61 @@
+var jade = require("jade");
+var path = __dirname + "/views";
+var url = require("url");
+
+var data = {
+    userdata: {
+		username: "per",
+		password: "123"
+    },  
+
+    blogposts: [
+	    { 
+			author : "per",
+			title  : "pers post",
+			text   : "pers text",
+			date   : "01/03/2015",
+		    image  : "http://icons.iconarchive.com/icons/custom-icon-design/pretty-office-9/128/square-icon.png"
+	 	},
+		{
+			author : "per2",
+			title  : "pers post 2",
+			text   : "pers text 2",
+			date   : "05/03/2015",
+			image  : "http://icons.iconarchive.com/icons/custom-icon-design/pretty-office-9/128/square-icon.png"  
+		}
+	]
+}
+
 module.exports = {
 
-	home: function handler(req, res, htmlOutput) {
+	home: function handler(req, res) {
+		var fn = jade.compileFile(path+"/index.jade");
+		var htmlOutput = fn(data);
 		console.log("Request handler 'home' was called.");
 		res.writeHead(200, {"Content-Type": "text/html"});
 		res.end(htmlOutput);
 	},
 
 	create: function handler(req, res) {
+		console.log("create"+req.url);
 		console.log("Request handler 'create' was called.");
 		res.writeHead(200, {"Content-Type": "text/html"});
-		res.end("Create Article");
+		res.end(htmlOutput);
 	},
 
 	update: function handler(req, res) {
+		console.log("update"+req.url);
 		console.log("Request handler 'update' was called.");
 		res.writeHead(200, {"Content-Type": "text/html"});
-		res.end("Update Article");
+		res.end(htmlOutput);
+	},
+	
+	addblog: function handler(req, res) {
+		var fn = jade.compileFile(path+req.url+".jade");
+		var htmlOutput = fn(data);
+		console.log("Request handler 'update' was called.");
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.end(htmlOutput);
 	}
 }
 
--- server.js
+++ server.js
@@ -2,42 +2,13 @@ var http = require("http");
 var ecstatic = require('ecstatic')({root: __dirname + '/public'});
 var handler = require("./handler");
 var port = 4000;
-var jade = require("jade");
-var path = __dirname + "/public/index.jade";
-var fn = jade.compileFile(path);
-
-var data = {
-    userdata: {
-		username: "per",
-		password: "123"
-    },  
-
-    blogposts: [
-	    { 
-		    author : "per",
-		    title  : "pers post",
-		    text   : "pers text",
-		    date   : "01/03/2015",
-		    image  : "www.google.com/image"  
-	 	}, 
-		{ 
-		    author : "per2",
-		    title  : "pers post 2",
-		    text   : "pers text 2",
-		    date   : "05/03/2015",
-		    image  : "www.google.com/image2"  
-		}
-	]
-}
-
-var htmlOutput = fn(data);
 
 //*** List of Routes and Associated Handler Functions ***//
 var routes = {}
 routes["/"] = handler.home;
 routes["/create"] = handler.create;
 routes["/update"] = handler.update;
-
+routes["/addblog"] = handler.addblog;
 
 //*** Invokes the right handler or throws error ***//
 var router = function(req, res){
@@ -45,7 +16,7 @@ var router = function(req, res){
 	console.log("request received for ", url);
 
 	if (typeof routes[url] === 'function'){
-		routes[url](req, res, htmlOutput);
+		routes[url](req, res);
 	} else {
 	    ecstatic(req, res);
 	}
